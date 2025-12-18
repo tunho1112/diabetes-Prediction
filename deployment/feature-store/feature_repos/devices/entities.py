822047091174ae73f26810abbb84edd14b9b6ccc
chinhpham204719 <chinhpham.204719@gmail.com>
2025-12-18 14:28:44 +0700

    modify: empty branch

--- deployment/feature-store/feature_repos/devices/entities.py
+++
@@ -1,7 +0,0 @@
-from feast import Entity
-
-device = Entity(
-    name="diabete",
-    join_keys=["diabete_id"],
-    description="diabete id",
-)
--- deployment/feature-store/feature_repos/devices/materialize.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-CURRENT_TIME=$(date +"%Y-%m-%dT%H:%M:%S")
-feast materialize-incremental $CURRENT_TIME
